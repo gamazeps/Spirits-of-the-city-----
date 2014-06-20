971f4d9a17ce95001aebf1d830a1a0949b823528
chikhaoui <aimen.chikhaoui@telecom-paristech.fr>
2014-06-20 18:20:57 +0200

    Correction des bugs lors du make pour le test de la SPI + nettoyage des parties inutiles du main.c

--- Test_RF/main.c
+++ Test_RF/main.c
@@ -1,97 +1,82 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+  ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
 
 #include "ch.h"
 #include "hal.h"
-#include "test.h"
 
-#include "lwipthread.h"
-
-#include "web/web.h"
-
-static void clear_all(){
-palClearPad(GPIOF,GPIOF_STAT1);
-palClearPad(GPIOF,GPIOF_STAT2);
-palClearPad(GPIOF,GPIOF_STAT3);
+static pwmcallback_t clear_all(void){
+  palClearPad(GPIOF,GPIOF_STAT1);
+  palClearPad(GPIOF,GPIOF_STAT2);
+  palClearPad(GPIOF,GPIOF_STAT3);
+  return 0;
 }
 
 static void set_green(PWMDriver *pwmd){
-palSetPad(GPIOF, GPIOF_STAT1);
+  (void)pwmd;
+  palSetPad(GPIOF, GPIOF_STAT1);
 }
 
 static void set_orange(PWMDriver *pwmd){
-palSetPad(GPIOF, GPIOF_STAT2);
+  (void)pwmd;
+  palSetPad(GPIOF, GPIOF_STAT2);
 }
 
 static void set_red(PWMDriver *pwmd){
-palSetPad(GPIOF, GPIOF_STAT3);
+  (void)pwmd;
+  palSetPad(GPIOF, GPIOF_STAT3);
 }
 /*
  * Green LED blinker thread, times are in milliseconds.
  */
- static PWMConfig pwmcfg = 
- {
-  200000,/*20 kHz clock frequency */
-  64, /*PWM period 0.005 second*/
-  clear_all, /*No callback*/
+static PWMConfig pwmcfg =
   {
-    {PWM_OUTPUT_DISABLED,set_green},
-    {PWM_OUTPUT_DISABLED,set_orange},
-    {PWM_OUTPUT_DISABLED,set_red},
-    {PWM_OUTPUT_DISABLED,NULL}
-  },
-0,0
-};
-
+    200000,/*20 kHz clock frequency */
+    64, /*PWM period 0.005 second*/
+    (pwmcallback_t) clear_all, /*No callback*/
+    {
+      {PWM_OUTPUT_DISABLED,set_green},
+      {PWM_OUTPUT_DISABLED,set_orange},
+      {PWM_OUTPUT_DISABLED,set_red},
+      {PWM_OUTPUT_DISABLED,NULL}
+    },
+    0,0
+  };
 
 static WORKING_AREA(waThread1, 128);
 static msg_t Thread1(void *arg) {
   (void)arg;
   chRegSetThreadName("blinker");
   while (TRUE) {
-   for(int i = 0;i<63;i++){
-    pwmEnableChannel(&PWMD5,0,64-i);
-    pwmEnableChannel(&PWMD5,1,64-i);
-    pwmEnableChannel(&PWMD5,2,64-i);
-    chThdSleepMilliseconds(50);
-  }
-  
-    /*for(i=0;i<20;i++){
-    palClearPad(GPIOF,GPIOF_STAT1);
-    palClearPad(GPIOF,GPIOF_STAT2);
-    palClearPad(GPIOF,GPIOF_STAT3);
-    chThdSleepMilliseconds(23-i);
-    palSetPad(GPIOF,GPIOF_STAT1);
-    palSetPad(GPIOF,GPIOF_STAT2);
-    palSetPad(GPIOF,GPIOF_STAT3);
-    chThdSleepMilliseconds(1+i);}
-    
-
-    for(i=0;i<20;i++){
-    palSetPad(GPIOF,GPIOF_STAT1);
-    palSetPad(GPIOF,GPIOF_STAT2);
-    palSetPad(GPIOF,GPIOF_STAT3);
-    chThdSleepMilliseconds(23-i);
-    palClearPad(GPIOF,GPIOF_STAT1);
-    palClearPad(GPIOF,GPIOF_STAT2);
-    palClearPad(GPIOF,GPIOF_STAT3);
-    chThdSleepMilliseconds(2+i);}*/
-    
+    for(int i = 0;i<63;i++){
+      pwmEnableChannel(&PWMD5,0,64-i);
+      pwmEnableChannel(&PWMD5,1,64-i);
+      pwmEnableChannel(&PWMD5,2,64-i);
+      chThdSleepMilliseconds(50);
     }
+  }
+  return 0;
 }
+
+static SPIConfig spi3cfg = {
+  NULL, // No callback
+  /* HW dependent part.*/
+  GPIOC, // CS is PC6
+  6, // CS is PC6
+  0x00000038 // CR1 : clock as low as possible 5:3=111
+};
 
 /*
  * Application entry point.
@@ -107,7 +92,8 @@ int main(void) {
    */
   halInit();
   chSysInit();
-  pwmStart(&PWMD5,&pwmcfg);
+  pwmStart(&PWMD5, &pwmcfg);
+  spiStart(&SPID3, &spi3cfg);
   /*
    * Activates the serial driver 6 using the driver default configuration.
    */
@@ -118,25 +104,12 @@ int main(void) {
    */
   chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 
-  /*
-   * Creates the LWIP threads (it changes priority internally).
-   */
-  chThdCreateStatic(wa_lwip_thread, LWIP_THREAD_STACK_SIZE, NORMALPRIO + 1,
-                    lwip_thread, NULL);
+  // test SPI
 
-  /*
-   * Creates the HTTP thread (it changes priority internally).
-   */
-  chThdCreateStatic(wa_http_server, sizeof(wa_http_server), NORMALPRIO + 1,
-                    http_server, NULL);
 
-  /*
-   * Normal main() thread activity, in this demo it does nothing except
-   * sleeping in a loop and check the button state.
-   */
+
+  // Go to bed
   while (TRUE) {
-    if (palReadPad(GPIOC, GPIOC_SWITCH_TAMPER) == 0)
-      TestThread(&SD6);
-    chThdSleepMilliseconds(500);
+    chThdSleepMilliseconds(1000);
   }
 }
