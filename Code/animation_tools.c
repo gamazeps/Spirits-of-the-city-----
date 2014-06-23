151d51108334dada39737c17df4cfc1dd233c9d4
CaroC <cabotcaroline@gmail.com>
2014-06-23 10:47:54 +0200

    Definition of clear_all function

--- Code/animation_tools.c
+++ Code/animation_tools.c
@@ -11,6 +11,24 @@
 static int CYCLIC_RATIO = 200; /* Number of states in PWM*/
 /* A FAIRE : CHOISIR LE TYPE. INT INNEFICACE (TROP DE BITS INUTILISES) */
 
+static void clear_all (PWMDriver *pwmd)
+{
+  palSetPad(GPIOA, GPIOA_UV1); 
+  palSetPad(GPIOA, GPIOA_UV2);
+  palSetPad(GPIOA, GPIOA_RGBbig_G);
+  palSetPad(GPIOB, GPIOB_RGBbig_R);
+  palSetPad(GPIOB, GPIOB_RGBbig_B);
+  palSetPad(GPIOA, GPIOA_RGBsmall_B);
+  palSetPad(GPIOB, GPIOB_RGBsmall_R);
+  palSetPad(GPIOB, GPIOB_RGBsmall_B);
+}
+
+static void set_heart_beat (PWMDriver *pwmd)
+{
+  palClearPad(GPIOF, GPIOF_STAT1);
+}
+
+
 static PWMConfig pwmcfg = {
   20000,
   CYCLIC_RATIO,
@@ -18,8 +36,7 @@ static PWMConfig pwmcfg = {
   {
     {PWM_OUTPUT_DISABLED, NULL},
     {PWM_OUTPUT_DISABLED, NULL},
-    {PWM_OUTPUT_DISABLED, NULL},
-    {PWM_OUTPUT_DISABLED, NULL},
+  
   },
   0,
   0
@@ -38,30 +55,32 @@ static void eyes_lightening (char eye, int8 blue, int8 red, int8 green ,int8 ini
   int t = time/CYCLIC_RATIO; 
   int sens = initial_intensity<final_intensity ? 1 : -1;
 
-  for(int i=1+(initial_intensity*CYCLIC_RATIO/10); i<CYCLIC_RATIO*final_intensity/10; i+=sens) {
+    for(int i=1+(initial_intensity*CYCLIC_RATIO/10); i<CYCLIC_RATIO*final_intensity/10; i+=sens) {
 
-    int c_bigblue = (eye = "2" | eye = "b") ? (i*blue)/256 : 0;
-    int c_bigred = (eye = "2" | eye = "b") ? (i*red)/256 : 0;
-    int c_biggreen = (eye = "2" | eye = "b") ? (i*green)/256 : 0;
-    int c_smallblue = (eye = "2" | eye = "s") ? (i*blue)/256 : 0;
-    int c_smallred = (eye = "2" | eye = "s") ? (i*red)/256 : 0;
-    int c_smallgreen = (eye = "2" | eye = "s") ? (i*green)/256 : 0;
+      int c_bigblue = (eye = "2" | eye = "b") ? (i*blue)/256 : 0;
+      int c_bigred = (eye = "2" | eye = "b") ? (i*red)/256 : 0;
+      int c_biggreen = (eye = "2" | eye = "b") ? (i*green)/256 : 0;
+      int c_smallblue = (eye = "2" | eye = "s") ? (i*blue)/256 : 0;
+      int c_smallred = (eye = "2" | eye = "s") ? (i*red)/256 : 0;
+      int c_smallgreen = (eye = "2" | eye = "s") ? (i*green)/256 : 0;
 
-    pwmEnableChannel(&PWMD3, 2, c_biggreen); 
-    pwmEnableChannel(&PWMD3, 3,c_bigblue);
-    pwmEnableChannel(&PWMD3, 4, c_bigred);
+      pwmEnableChannel(&PWMD3, 2, c_biggreen); 
+      pwmEnableChannel(&PWMD3, 3,c_bigblue);
+      pwmEnableChannel(&PWMD3, 4, c_bigred);
 
-    pwmEnableChannel(&PWMD2, 2, c_smallgreen);
-    pwmEnableChannel(&PWMD2, 1,c_smallblue);
-    pwmEnableChannel(&PWMD3, 1, c_smallred);	
+      pwmEnableChannel(&PWMD2, 2, c_smallgreen);
+      pwmEnableChannel(&PWMD2, 1,c_smallblue);
+      pwmEnableChannel(&PWMD3, 1, c_smallred);	
 
-    chThdSleepMilliseconds(t);  
-  }
+      chThdSleepMilliseconds(t);  
+    }
 };
 
 static void accelerate_heart_beats (int initial_speed, int final_speed, int time) = {
   int t = time/CYCLIC_RATIO; 
-  int sens = initial_intensity<final_intensity ? 1 : -1;
+  int sens = initial_speed<final_speed ? 1 : -1;
+
+
 
 }
 
